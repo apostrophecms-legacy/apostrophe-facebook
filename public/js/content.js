c47d157ee1a6d9de9b7120dda57650181fa7cd13
Joel Smith <jsumnersmith@gmail.com>
2014-11-10 12:38:55 -0500

    Removed client side logic about dates

--- public/js/content.js
+++ public/js/content.js
@@ -64,16 +64,6 @@ apos.widgetPlayers.facebook = function($el) {
         );
       }
 
-      function getFacebookDate(date){
-        var postDate = moment(date, 'MM-DD-YYYY'),
-            postMonth = postDate.month(),
-            postDay = postDate.date(),
-            postYear = postDate.year(),
-            thisYear = moment().year();
-        return ((postYear != thisYear ) ? postMonth +"/"+postDay+"/"+postYear: postMonth +"/"+postDay);
-      }
-
-
       function generatePostMarkup(posts){
         _.each(posts, function(post){
           //Clone our Template
@@ -93,8 +83,7 @@ apos.widgetPlayers.facebook = function($el) {
 
           //Add Date
           if(post.date){
-            var postDate = getFacebookDate(post.date);
-            $post.$date.text(postDate || '');
+            $post.$date.text(post.date || '');
           } else {
             $post.$date.remove();
           }
@@ -132,53 +121,6 @@ apos.widgetPlayers.facebook = function($el) {
       init();
     }
   );
-
-  Date.fromISO = (function(){
-    var testIso = '2011-11-24T09:00:27+0200';
-    // Chrome
-    var diso= Date.parse(testIso);
-    if(diso===1322118027000) return function(s){
-        return new Date(Date.parse(s));
-    }
-    // JS 1.8 gecko
-    var noOffset = function(s) {
-      var day= s.slice(0,-5).split(/\D/).map(function(itm){
-        return parseInt(itm, 10) || 0;
-      });
-      day[1]-= 1;
-      day= new Date(Date.UTC.apply(Date, day));
-      var offsetString = s.slice(-5)
-      var offset = parseInt(offsetString,10)/100;
-      if (offsetString.slice(0,1)=="+") offset*=-1;
-      day.setHours(day.getHours()+offset);
-      return day.getTime();
-    }
-    if (noOffset(testIso)===1322118027000) {
-       return noOffset;
-    }
-    return function(s){ // kennebec@SO + QTax@SO
-        var day, tz,
-        rx = /^(\d{4}\-\d\d\-\d\d([tT][\d:\.]*)?)([zZ]|([+\-])(\d\d):?(\d\d))?$/,
-
-        p= rx.exec(s) || [];
-        if(p[1]){
-            day= p[1].split(/\D/).map(function(itm){
-                return parseInt(itm, 10) || 0;
-            });
-            day[1]-= 1;
-            day= new Date(Date.UTC.apply(Date, day));
-            if(!day.getDate()) return NaN;
-            if(p[5]){
-                tz= parseInt(p[5], 10)/100*60;
-                if(p[6]) tz += parseInt(p[6], 10);
-                if(p[4]== "+") tz*= -1;
-                if(tz) day.setUTCMinutes(day.getUTCMinutes()+ tz);
-            }
-            return day;
-        }
-        return NaN;
-    }
-})()
 }
 
 apos.widgetPlayers.facebook.afterLoad = function($el, posts){
